d12f20a548a46a569a70c1441a2788fdc4127228
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2022-11-08 19:54:46 -0800

    yab

--- test.rb
+++ test.rb
@@ -1,3 +1,9 @@
-for i in 0..5
-  puts "Value of local variable is #{i}"
+def test
+  puts "You are in the method"
+  yield
+  puts "You are again back to the method"
+  yield
 end
+test {puts "You are in the block"}
+
+#yield like exits the function and when called again, starts from where it was left off
